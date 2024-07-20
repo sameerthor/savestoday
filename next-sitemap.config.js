7904009e81910c0d82f89430a36cfb663beba800
sameerthor <sameer@thor.solutions>
2024-07-20 14:32:42 +0530

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://www.anddeals.com',
+    generateRobotsTxt: false, // (optional)
+    generateIndexSitemap: false,
+    transform: async (config, path) => {
+        return {
+            loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+            changefreq: "daily",
+            lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        }
+    }
+}
