7c77780548f40bcb7a130e9bfc0adce9aa484cf3
sameerthor <sameer@thor.solutions>
2024-08-21 11:31:40 +0530

    revalidate time

--- src/pages/index.js
+++ src/pages/index.js
@@ -239,6 +239,6 @@ export async function getStaticProps({ params }) {
         // Next.js will attempt to re-generate the page:
         // - When a request comes in
         // - At most once every 10 seconds
-        revalidate: 10, // In seconds
+        revalidate: 20, // In seconds
     }
 }
